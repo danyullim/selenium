30af8f599046c5e9fc96fc0175ee1f60b433653e
danyullim <daniel.taeyounglim@gmail.com>
2020-11-24 01:46:44 -0600

    new stuff

--- church.py
+++ church.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.key import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver.support.ui import Select
 
+# Locating path to directory of the chromedriver.exe file
 PATH = "/Users/danyullim/Github/selenium/chromedriver"
+# Set up the driver using chrome
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://danyullim.github.io")
+# Accessing church webpage
+driver.get("http://seoulbaptist.org/")
 print(driver.title)
 
-search = driver.find_element_by_name("")
+search = driver.find_element_by_name("covid_01")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
