1471418ed427945187b5c3d08b0b140ad0254606
danyullim <daniel.taeyounglim@gmail.com>
2020-11-24 14:40:46 -0600

    new website

--- church.py
+++ church.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
-# from selenium.webdriver.common.key import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+import time
 
 # Locating path to directory of the chromedriver.exe file
 PATH = "/Users/danyullim/Github/selenium/chromedriver"
@@ -14,15 +11,5 @@ driver = webdriver.Chrome(PATH)
 driver.get("http://seoulbaptist.org/")
 print(driver.title)
 
-search = driver.find_element_by_name("covid_01")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
-
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
-    )
-except:
-    driver.quit()
-
-print(main.text)
+link = driver.find_element_by_css_selector("area.rect")
+link.click()
