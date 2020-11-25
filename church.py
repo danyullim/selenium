91b6548006491894c5a2d128ee05e34cdcc4646b
danyullim <daniel.taeyounglim@gmail.com>
2020-11-25 01:32:25 -0600

    final updates

--- church.py
+++ church.py
@@ -1,18 +1,71 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
 # Locating path to directory of the chromedriver.exe file
 PATH = "/Users/danyullim/Github/selenium/chromedriver"
 # Set up the driver using chrome
-driver = webdriver.Chrome(PATH)
+web = webdriver.Chrome(PATH)
+# Accessing church's homepage
+web.get('desiredURL')
 
-# Accessing church webpage
-driver.get("http://seoulbaptist.org/")
-print(driver.title)
+# Allow the form to load 
+time.sleep(2)
 
-link = driver.find_element_by_css_selector("area")
-link.click()
+##### FIRST PAGE – EMAIL SECTION #####
+# Input this email
+emailInput = input("What is your email you want to input: \n")
+# Find element with the XPATH
+email = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+# Fill in with the emailInput
+email.send_keys(emailInput)
+# Locate Submit Button
+firstNext = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+# Click Submit Button
+firstNext.click()
 
-email = driver.find_element_by_xpath('/html/body/div/div[2]/form/div[2]/div/div[2]/div/div/div[1]/div[2]/div[1]/div/div[1]/input')
-email.click()
+# Allow the form to load 
+time.sleep(2)
+
+##### SECOND PAGE – NUMBER OF ATENDEES #####
+# Locate the participants button
+participantsButton = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+participantsButton.click()
+# Locate next button
+secondNext = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+secondNext.click()
+
+# Allow the form to load 
+time.sleep(2)
+
+##### THIRD PAGE - NAME OF ATENDEES #####
+# The names needed to input
+name1 = "What is your name that you want to input: \n"
+name2 = "Who is your +1?: \n"
+# Locate and type the first atendee box
+name1Box = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+name1Box.send_keys(name1)
+# Locate and type the second atendee box
+name2Box = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+name2Box.send_keys(name2)
+# Locate the submit button
+lastNext = web.find_element_by_xpath('inspect elements and copy the XPATH and paste it here')
+lastNext.click()
+
+# Allow the form to load 
+time.sleep(2)
+
+##### FOURTH PAGE – Service Time? #####
+# Locate 1st Service Button
+serviceButton = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+serviceButton.click()
+# Locate the submit button
+finalButton = web.find_element_by_css_selector('inspect elements and copy the css selector and paste it here')
+finalButton.click()
+
+
+
+
+
+
+
+
